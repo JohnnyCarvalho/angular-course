cc3de03e439d91564076b34d5174199469ef1355
Johnny Carvalho <Johnny.Carvalho@ibm.com>
2022-09-23 10:37:10 -0300

    commit
    atualizando os comentários

--- my-first-project/data-building/src/app/app.component.ts
+++ my-first-project/data-building/src/app/app.component.ts
@@ -1,3 +1,33 @@
+/**
+ * O data binding nada mais é do que uma forma de exibir dados em um componente HTML, ou 
+ * trabalhar, manipular dados em um componente HTML.
+ * Desta forma temos então o:
+ * 
+ * - Interpolatiion: Permite que você incorpore valores dinâmicos dentro de seu modelo HTML ex: 
+ *    componente TS -> public nome: string = 'Johnny';
+ *    componente HTML -> <h1> {{ nome }} </h1> - Interpolation
+ * 
+ * - Property Binding: ajuda a definir valores para propriedades de elementos ou diretivas HTML
+ *    componente TS -> public img: string = './imagem.jpj';
+ *    componente HTML -> <img [src]="img">, outro exemplo, <button [disabled]=""disabeButton>Button</button>
+ *    oque define um elemento property Binding é o uso de colchetes [] conforme exemplos acima.
+ * 
+ * - Event Binding: é a associação de eventos que permite você escutar e responder as ações do usuário
+ *   como precionamento de tecla, movimentos do mouse, cliques e toques.
+ *    componente HTML -> <button (Click)=""calc>Button</button>
+ *    oque define um elemento property Binding é o uso de chaves () conforme exemplos acima.
+ * 
+ * - Two Way Bonding: é a união do Property Binding com o Event Binding, usado para ouvir eventos e 
+ *   atualizar valores simultaneamente entre componentes pai e filho
+ *    componente TS:
+ *      public nome: string = 'Johnny';
+ *    componente HTML:
+ *      <input [(ngModel)]="nome" -> toda vez que o input for modificado, a variável vai receber o novo valor digitado pelo usuário.
+ *      <span> {{ nome }} </span>
+ * 
+ */
+
+
 import { Component } from '@angular/core';
 
 @Component({
