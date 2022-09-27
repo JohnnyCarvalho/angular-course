eda5c8df3498e4ac42b25e3ab75fac0a3ecb7e0e
Johnny Carvalho <johnnycarvalhoem@gmail.com>
2022-09-26 21:19:54 -0300

    commit
    Aula sobre @Input concluída
    
    Co-Authored-By: JohnnyCarvalhoDev <johnnycarvalhodev@gmail.com>

--- comunicacao-entre-componentes/src/app/app.component.ts
+++ comunicacao-entre-componentes/src/app/app.component.ts
@@ -2,9 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+  <app-input [contador]="numero"></app-input>
+<button (click)="insert()">Insert</button>
+  <router-outlet></router-outlet>`
 })
+
 export class AppComponent {
-  title = 'comunicacao-entre-componentes';
+  public numero: number = 0;
+  public destroi: boolean = true;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  public insert = () => {
+    this.numero += 1;
+  }
+
+  public destruir = () => {
+    this.destroi = false;
+  }
 }
