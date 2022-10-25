db1c6bb723e261892dc05c798dca6827062c7e5e
Johnny Carvalho <johnnycarvalhoem@gmail.com>
2022-10-25 10:12:42 -0300

    commit

---
+++ 014-third-project-login-auth/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '014-third-project-login-auth';
+}
